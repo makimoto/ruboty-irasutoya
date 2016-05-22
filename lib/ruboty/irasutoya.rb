6c00d36e7e698802a31d307573a05b190ef12997
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-05-22 22:17:50 +0900

    Implement

---
+++ lib/ruboty/irasutoya.rb
@@ -0,0 +1,28 @@
+require "open-uri"
+require "nokogiri"
+
+module Ruboty
+  module Handlers
+    class Irasutoya < Base
+
+      SEARCH_ENDPOINT = "http://www.irasutoya.com/search?q="
+
+      on(
+        /irasutoya(?:\s+(.*))?/,
+        name: 'irasutoya',
+        description: 'show irasuto from irasutoya'
+      )
+
+      def irasutoya(message)
+        keyword = message.match_data[1]
+        nokogiri = Nokogiri::HTML(open("#{SEARCH_ENDPOINT}#{URI.escape(keyword)}"))
+        result = nokogiri.xpath("//div[@class='boxim']/a").to_a.sample
+        if result
+          message.reply(result.attr('href'))
+        else
+          message.reply('ಠ_ಠ')
+        end
+      end
+    end
+  end
+end
---
+++ lib/ruboty/irasutoya/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Irasutoya
+    VERSION = "0.1.0"
+  end
+end
